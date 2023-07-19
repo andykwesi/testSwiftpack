071f53e1f42ee7d86ac4df4232516bcfaa68b340
Andy Kwesi Apenteng <36931378+andykwesi@users.noreply.github.com>
2023-07-19 08:11:49 +0000

    Initial Commit

---
+++ testSwiftPackage/testSwiftPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftPackageApp.swift
+//  testSwiftPackage
+//
+//  Created by Andy Kwesi on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
